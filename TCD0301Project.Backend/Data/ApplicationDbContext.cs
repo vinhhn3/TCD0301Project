c526a90abfe383524fb59d10061bb16af4c2555d
Vinh Hoang <vinhhn3@fe.edu.vn>
2021-12-01 13:34:42 +0700

    Setup the Database connection

---
+++ TCD0301Project.Backend/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TCD0301Project.Backend.Models;
+
+namespace TCD0301Project.Backend.Data
+{
+  public class ApplicationDbContext : DbContext
+  {
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+      : base(options)
+    {
+
+    }
+
+    public DbSet<Park> Parks { get; set; }
+  }
+}
--- TCD0301Project.Backend/Startup.cs
+++ TCD0301Project.Backend/Startup.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -10,6 +11,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TCD0301Project.Backend.Data;
 
 namespace TCD0301Project.Backend
 {
@@ -25,6 +27,11 @@ namespace TCD0301Project.Backend
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
+      // Setup the connectionString
+      services.AddDbContext<ApplicationDbContext>(options =>
+        options.UseSqlServer(
+          Configuration.GetConnectionString("DefaultConnection"))
+      );
       services.AddControllers();
     }
 
