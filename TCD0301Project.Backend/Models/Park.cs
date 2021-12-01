40f1f646bc24d054320592444758e3f34de3ea8a
Vinh Hoang <vinhhn3@fe.edu.vn>
2021-12-01 13:22:25 +0700

    Add Park model

---
+++ TCD0301Project.Backend/Models/Park.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TCD0301Project.Backend.Models
+{
+  public class Park
+  {
+    [Key]
+    public int Id { get; set; }
+    [Required]
+    [StringLength(255)]
+    public string Name { get; set; }
+    public string State { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+    public DateTime Established { get; set; }
+  }
+}
