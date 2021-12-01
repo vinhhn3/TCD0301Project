97c03adb8d2e436e1241dc9d88bb843370f81e20
Vinh Hoang <vinhhn3@fe.edu.vn>
2021-12-01 13:51:51 +0700

    Add ParksController: building a simple API

---
+++ TCD0301Project.Backend/Controllers/ParksController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Net;
+using TCD0301Project.Backend.Data;
+
+namespace TCD0301Project.Backend.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class ParksController : ControllerBase
+  {
+    private ApplicationDbContext _context;
+    public ParksController(ApplicationDbContext context)
+    {
+      _context = context;
+    }
+
+    [HttpGet]
+    public IActionResult GetParks()
+    {
+      var parks = _context.Parks.ToList();
+      return Ok(parks);
+    }
+  }
+}
