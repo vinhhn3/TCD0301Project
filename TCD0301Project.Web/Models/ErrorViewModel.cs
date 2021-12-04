0d069cde7f4e797d48cfceeab899243f944277e0
Vinh Hoang <vinhhn3@fe.edu.vn>
2021-12-04 07:58:42 +0700

    Init the front end

---
+++ TCD0301Project.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TCD0301Project.Web.Models
+{
+  public class ErrorViewModel
+  {
+    public string RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+  }
+}
